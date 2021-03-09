c2b803185a4cb3154c8fb5551de61756594973b0
Venkata Subbaiah Gummadi <vg19434@corp.ojas-it.com>
2021-03-09 11:05:22 +0530

    new project

---
+++ SpringBootBasicProject/src/main/java/com/example/demo/SpringBootBasicProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBasicProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBasicProjectApplication.class, args);
+		System.out.println("hello world");
+	}
+}
